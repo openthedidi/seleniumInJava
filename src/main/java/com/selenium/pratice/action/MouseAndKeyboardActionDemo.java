1988af18f8343b04bb29edf99917a156c63b1116
openthedidi <openthedidi2004@gmail.com>
2026-02-11 13:28:58 +0800

    [update]:新增滑鼠 drag and drop

--- src/main/java/com/selenium/pratice/action/MouseAndKeyboardActionDemo.java
+++ src/main/java/com/selenium/pratice/action/MouseAndKeyboardActionDemo.java
@@ -7,13 +7,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class MouseAndKeyboardActionDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
@@ -35,7 +36,30 @@ public class MouseAndKeyboardActionDemo {
         action.keyDown(searchInput, Keys.SHIFT).sendKeys("abc").perform();
         action.keyDown(searchInput,Keys.CONTROL).keyDown("a").keyUp(Keys.CONTROL).perform();
         action.sendKeys(Keys.DELETE).perform();
+        Thread.sleep(3000);
         action.sendKeys(searchInput,"台北").perform();
+
+        /// Drag And Drop
+        driver.get("https://www.globalsqa.com/demo-site/draganddrop/");
+
+
+
+        Wait<WebDriver> waitFlunt = new FluentWait<>(driver)
+                .pollingEvery(Duration.ofSeconds(2))
+                .withTimeout(Duration.ofSeconds(10));
+        WebElement iframe =waitFlunt.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@class='demo-frame']"))
+        );
+        driver.switchTo().frame(iframe);
+
+
+        WebElement dragTarget = waitFlunt.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//h5[text()='High Tatras']"))
+        );
+        WebElement dropTarget = waitFlunt.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='trash']")));
+
+        action.dragAndDrop(dragTarget,dropTarget).perform();
 
     }
 }
